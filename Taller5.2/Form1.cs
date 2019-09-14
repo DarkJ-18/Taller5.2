7cbcca8abbc7f77543cd86c92475e2d0b3aa37e4
Juan <j.juancamilojurado@gmail.com>
2019-09-14 00:57:23 -0500

    Initial commit

---
+++ Taller5.2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller5._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            int num = int.Parse(txtnumeros.Text);
+           
+            int i = 1;
+
+            listBox1.Items.Clear();
+
+            while (i <= num)
+            {
+                listBox1.Items.Add(i++);
+             
+            }
+
+
+
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
